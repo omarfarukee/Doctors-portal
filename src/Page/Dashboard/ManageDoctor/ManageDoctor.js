3f98de2c90060dc29312c4a56349a23217813cad
omarfarukee <umarfaruque1920@gmail.com>
2022-12-05 09:45:59 +0600

    portal commit

--- src/Page/Dashboard/ManageDoctor/ManageDoctor.js
+++ src/Page/Dashboard/ManageDoctor/ManageDoctor.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ManageDoctor = () => {
     return (
         <div>
-            <h1 className='text-2xl'>manageDoctor</h1>
+            <h1 className='text-3xl'>manageDoctor</h1>
         </div>
     );
 };
